3e7afbb7a7c38341556c7f7edb81a8696c589961
kenta-afk <kentarou9.60@icloud.com>
2024-08-10 15:58:26 +0900

    add Edit

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,1 +1,41 @@
-<template></template>
+<script setup>
+import { ref } from "vue";
+import { statuses } from "../const/status";
+
+const input = ref("");
+const inputDate = ref("");
+
+const isErrMsg = ref(false);
+
+function onSubmitForm() {
+    if (input.value=="" || inputDate.value==""){
+        isErrMsg.value = true;
+        event.preventDefault();
+        return;
+    }
+    const items = JSON.parse(localStorage.getItem("items")) || [];
+
+    const newItem = {
+        id: items.length,
+        content: input.value,
+        limit: inputDate.value,
+        state: statuses.NOT_START,
+        onEdit: false,
+    };
+
+    items.push(newItem);
+
+    localStorage.setItem("items", JSON.stringify(items));
+}
+</script>
+
+<template>
+    <div>
+        <p v-if="isErrMsg">タスク・期限を両方入力してください。</p>
+        <form @submit="onSubmitForm">
+            <label>やること<input type="text" v-model="input" /></label>
+            <label>期限<input type="date" v-model="inputDate" /></label>
+            <input type="submit" value="登録！" />
+        </form>
+    </div>
+</template>
--- src/components/TodoListView.vue
+++ src/components/TodoListView.vue
@@ -1,1 +1,86 @@
-<template></template>
+<script setup>
+import { ref } from "vue";
+import { statuses } from "../const/status"
+
+let items = ref(JSON.parse(localStorage.getItem("items")) || []);
+let inputContent = ref();
+let inputLimit = ref();
+let inputState = ref();
+
+let isErrMsg = ref(false);
+
+
+
+function onEdit(id) {
+    inputContent.value = items.value[id].content;
+    inputLimit.value = items.value[id].limit;
+    inputState.value = items.value[id].state;
+    items.value[id].onEdit = true;
+}
+
+function onUpdate(id) {
+    if (inputContent.value == "" || inputLimit.value == "") {
+        isErrMsg.value = true;
+        return;
+    }
+
+    const newItem = {
+        id: id,
+        content: inputContent.value,
+        limit: inputLimit.value,
+        state: inputState.value,
+        onEdit: false,
+    };
+
+    items.value.splice(id, 1, newItem);
+
+    localStorage.setItem("items", JSON.stringify(items.value));
+
+    isErrMsg.value = false;
+}   
+</script>
+
+<template>
+    <p v-if="isErrMsg">タスク・期限を両方入力してください。</p>
+    <div>
+        <table>
+            <tr>
+                <th class="th-id">ID</th>
+                <th class="th-value">やること</th>
+                <th class="th-limit">期限</th>
+                <th class="th-state">状態</th>
+                <th class="th-edit">編集</th>
+                <th class="th-delete">削除</th>
+            </tr>
+            <tr v-for="item in items" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>
+                    <span v-if="!item.onEdit">{{ item.content }}</span>
+                    <input v-else v-model="inputContent" type="text" />
+                </td>
+                <td>
+                    <span v-if="!item.onEdit">{{ item.limit }}</span>
+                    <input v-else v-model="inputLimit" type="date" />
+                </td>
+                <td>
+                    <span v-if="!item.onEdit">{{ item.state.value }}</span>
+                    <select v-else v-model="inputState">
+                        <option
+                            v-for="state in statuses"
+                            :key="state.id"
+                            :value="state"
+                            :selected="state.id == item.state.id"
+                        >
+                            {{ state.value }}
+                        </option>
+                    </select>
+                </td>
+                <td>
+                    <button v-if="!item.onEdit" @click="onEdit(item.id)">編集</button>
+                    <button v-else @click="onUpdate(item.id)">完了</button>
+                </td>
+                <td><button>削除</button></td>
+            </tr>
+        </table>
+    </div>
+</template>
